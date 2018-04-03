33fa7cf23f6ebfc634b4c11036b0b337e1b179aa
Diharaw <shredhead94@gmail.com>
2018-04-03 22:41:42 +0530

    Begin progress on render graph system.

---
+++ src/1_pbr_demo/render_node.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <stdint.h>
+#include <string>
+
+struct Texture;
+struct Framebuffer;
+
+namespace dw
+{
+	class Scene;
+	class Camera;
+	class RenderDevice;
+	class Renderer;
+
+	class RenderNode
+	{
+	public:
+		static RenderNode* load(std::string json,
+								RenderDevice* device,
+								Renderer* renderer,
+								uint16_t w,
+								uint16_t h);
+
+		void execute(Scene* scene, Camera* camera);
+		void create_render_targets(uint16_t w, uint16_t h);
+
+	private:
+		struct Input
+		{
+			std::string id;
+			Texture* texture;
+			int binding;
+		};
+
+		Framebuffer* m_fbo;
+		int			 m_num_render_target_inputs;
+		Input		 m_render_target_inputs[8];
+		int			 m_num_texture_inputs;
+		Input		 m_texture_inputs[8];
+	};
+}
